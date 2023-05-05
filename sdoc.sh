d634fa7f644cd6ecc7e4775751d6c56617f790c1
gasts <gast.steven@gmail.com>
2023-05-05 15:44:07 +0200

    Changes to be committed: 	renamed:    examples/powi/ab.md -> examples/default/ab.md 	renamed:    examples/powi/ab.pdf -> examples/default/ab.pdf 	renamed:    examples/powi/assets/abbildung01.png -> examples/default/assets/abbildung01.png 	renamed:    examples/powi/test -> examples/default/test 	new file:   examples/eisvogel/document.md 	modified:   sdoc.sh 	new file:   templates/eisvogel/eisvogel.latex

--- sdoc.sh
+++ sdoc.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-TEMPLTE_FOLDER_PATH="$HOME/.pandoc/templates/"
+SYS_TEMPLTE_PATH="$HOME/.pandoc/templates/"
 
-GIT_LETTER_TEMPLATE="https://raw.githubusercontent.com/gasts/pandoc-markdown/main/templates/letter-din5008/letter.latex?token=GHSAT0AAAAAAB77JDDYINXJNFBLEQNLJBRMZCUB4YQ"
-LETTER_FILE_NAME="letter.latex"
+LETTER_TEMPLATE_GIT="https://raw.githubusercontent.com/gasts/pandoc-markdown/main/templates/letter-din5008/letter.latex?token=GHSAT0AAAAAAB77JDDYINXJNFBLEQNLJBRMZCUB4YQ"
+LETTER_TEMPLATE_NAME="letter.latex"
+LETTER_EXAMPLE_GIT="https://raw.githubusercontent.com/gasts/pandoc-markdown/main/examples/letter-din5008/letter-minimal/letter.md?token=GHSAT0AAAAAACBVW7FYRRHQPLM63ZQIZG22ZCVBZIA"
+INVOICE_EXAMPLE_GIT="https://raw.githubusercontent.com/gasts/pandoc-markdown/main/examples/letter-din5008/letter-invoice/letter.md?token=GHSAT0AAAAAACBVW7FZ3ECI6PGP75HIPZHOZCVCNUQ"
+
+EISVOGEL_TEMPLATE_GIT=""
+EISVOGEL_TEMPLATE_NAME="eisvogel.latex"
+EISVOGEL_EXAMPLE_GIT=""
+
+DEFAULT_TEMPLATE_GIT=""
+DEFAULT_TEMPLATE_NAME="default.latex"
+DEFAULT_EXAMPLE_GIT=""
+
+FORMAT="%-30s %s\n"
 
 # Create template directory if they don't exist
 create_template_directory(){
-    if [ -d "$TEMPLTE_FOLDER_PATH" ]; then
+    if [ -d "$SYS_TEMPLTE_PATH" ]; then
         echo "Template directory already exists"
     else
-        echo "Create $TEMPLTE_FOLDER_PATH"
-        mkdir -p $TEMPLTE_FOLDER_PATH
+        echo "Create $SYS_TEMPLTE_PATH"
+        mkdir -p $SYS_TEMPLTE_PATH
     fi
 }
 
 # Function to check if a pip package is installed
-is_package_installed() {
+is_pip_package_installed() {
     if pip3 show $1 &> /dev/null; then
         return 0
     else
@@ -37,20 +49,31 @@ is_pip_installed() {
 # Update 
 install_update_templates(){
     echo "Download and Install template files"
-    if [ -e $TEMPLTE_FOLDER_PATH/$LETTER_FILE_NAME ]; then
-        # backup last old file
-        mv $TEMPLTE_FOLDER_PATH/$LETTER_FILE_NAME $TEMPLTE_FOLDER_PATH/$LETTER_FILE_NAME.bak
-        curl -L $GIT_LETTER_TEMPLATE -o "$TEMPLTE_FOLDER_PATH/$LETTER_FILE_NAME"
-    else
-        curl -L $GIT_LETTER_TEMPLATE -o "$TEMPLTE_FOLDER_PATH/$LETTER_FILE_NAME"
+    # letter template
+    if [ -e $SYS_TEMPLTE_PATH/$LETTER_TEMPLATE_NAME ]; then
+        echo "Backup $SYS_TEMPLTE_PATH/$LETTER_TEMPLATE_NAME to $SYS_TEMPLTE_PATH/$LETTER_TEMPLATE_NAME.bak"
+        mv $SYS_TEMPLTE_PATH/$LETTER_TEMPLATE_NAME $SYS_TEMPLTE_PATH/$LETTER_TEMPLATE_NAME.bak
     fi
+    # eisvogel template
+    if [ -e $SYS_TEMPLTE_PATH/$EISVOGEL_TEMPLATE_GIT ]; then
+        echo "Backup $SYS_TEMPLTE_PATH/$EISVOGEL_TEMPLATE_NAME to $SYS_TEMPLTE_PATH/$EISVOGEL_TEMPLATE_NAME.bak"
+        mv $SYS_TEMPLTE_PATH/$EISVOGEL_TEMPLATE_NAME $SYS_TEMPLTE_PATH/$EISVOGEL_TEMPLATE_NAME.bak
+    fi
+    # default template
+    if [ -e $SYS_TEMPLTE_PATH/$DEFAULT_TEMPLATE_NAME ]; then
+        echo "Backup $SYS_TEMPLTE_PATH/$DEFAULT_TEMPLATE_NAME to $SYS_TEMPLTE_PATH/$DEFAULT_TEMPLATE_NAME.bak"
+        mv $SYS_TEMPLTE_PATH/$DEFAULT_TEMPLATE_NAME $SYS_TEMPLTE_PATH/$DEFAULT_TEMPLATE_NAME.bak
+    fi
+    curl -O $LETTER_TEMPLATE_GIT
+    curl -O $EISVOGEL_TEMPLATE_GIT
+    curl -O $DEFAULT_TEMPLATE_GIT
 }
 
 # Parse command line arguments
 if [ "$1" == "--install" ]; then
     # Check if requirements are installed
     if is_pip_installed; then
-        if is_package_installed "pandoc-latex-environment"; then
+        if is_pip_package_installed "pandoc-latex-environment"; then
             echo "pandoc-latex-environment is already installed"
         else
             # Install the package
@@ -64,16 +87,24 @@ if [ "$1" == "--install" ]; then
     install_update_templates
 elif [ "$1" == "--new" ]; then
     echo "Creating file and folder structur"
+    echo "Create assets directory"
     mkdir assets
     if [ -n "$2" ] && [ "$2" == "letter" ]; then
-        echo "Create letter template"
-
+        echo "Create letter.md template"
+        curl -O $LETTER_EXAMPLE_GIT
     elif [ -n "$2" ] && [ "$2" == "eisvogel" ]; then
         echo "Create eisvogel template"
-        
+        curl -O $EISVOGEL_EXAMPLE_GIT
     else
         echo "Create default template";
-
+        curl -O $DEFAULT_EXAMPLE_GIT
+    fi
+elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    echo ""
+    echo "Usage:"
+    printf "${FORMAT}" "sdoc --install" "install requirements"
+    printf "${FORMAT}" "sdoc --new" "create default project"
+    printf "${FORMAT}" "sdoc --new <typ>" "typ is letter, invoice or eisvogel"
 else
     echo "$1: invalid option"
     echo "Try '--help' for more information."
